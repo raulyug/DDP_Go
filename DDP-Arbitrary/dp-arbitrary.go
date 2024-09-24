6f89afe9e85b1a47428f63e6086d97ab5ceba3f0
raulyug <raulyugueros@yahoo.com.br>
2024-09-24 14:02:22 -0300

    Removed Few Comments

--- DDP-Arbitrary/dp-arbitrary.go
+++ DDP-Arbitrary/dp-arbitrary.go
@@ -9,34 +9,29 @@ const numGarfos = 5
 
 func filosofo(id int, garcom chan int) {
 	for i := 0; i < 3; i++ {
-		// Tenta pegar dois garfos
-		fmt.Printf("Filósofo %d pediu permissao p/ pegar 2 garfos...\n", id)
-		garcom <- 1 
+		fmt.Printf("Filósofo %d pediu permissao p/ pegar garfos\n", id)
+		garcom <- 1
 		garcom <- 1
 
-		fmt.Printf("Garcom cedeu filosofo %d  p/ pegar os 2 garfos.\n", id)
-		fmt.Printf("Filósofo %d está comendo.\n", id)
-		time.Sleep(time.Second * 2) 
+		fmt.Printf("Garcom cedeu filosofo %d p/ pegar os garfos.\n", id)
+		fmt.Printf("Filósofo %d está comendo pela %d vez.\n", id, i+1)
+		time.Sleep(time.Second * 2)
 
-		// Devolve os dois garfos
-		<-garcom 
-		<-garcom 
-		
+		<-garcom
+		<-garcom
 		fmt.Printf("Filósofo %d terminou de comer e devolveu os garfos.\n", id)
-	
 
-		// O filósofo volta a pensar 
-		time.Sleep(time.Second * 2) 
+		// O filósofo volta a pensar
+		time.Sleep(time.Second * 2)
 	}
 }
 
 func main() {
-
+	const numPhilosophers = 5
 	garcom := make(chan int, numGarfos)
 
-
-	for i := 1; i <= 5; i++ {
-	 filosofo(i, garcom)
+	for i := 1; i <= numPhilosophers; i++ {
+		go filosofo(i, garcom)
 	}
 
 	// Evita que o programa principal finalize antes das goroutines
